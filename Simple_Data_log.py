42fa79cda2b6dbf87f4df59ff57853bdca76dd40
Ellen-E-Bailey <ellenbailey7@btinternet.com>
2026-03-10 11:33:46 +0000

    Increased length of msg to include battery status and throttle values

--- Simple_Data_log.py
+++ Simple_Data_log.py
@@ -23,19 +23,32 @@ log_filename = f"logs/mavlog_{datetime.now().strftime('%Y%m%d_%H%M%S')}.csv"
 
 with open(log_filename, mode='w', newline='') as logfile:
     writer = csv.writer(logfile)
-    writer.writerow(["timestamp", "msg_type", "lat", "lon", "alt", "roll", "pitch", "yaw"])
+    writer.writerow([
+        "timestamp",
+        "msg_type",
+        "lat", "lon", "alt",
+        "roll", "pitch", "yaw",
+        "voltage", "current", "battery_remaining",
+        "throttle"
+    ])
+
 
     print(f"Logging to {log_filename}")
 
     # --- get msg ---
     while True:
-        msg = connection.recv_match(type=['GLOBAL_POSITION_INT', 'ATTITUDE'], blocking=False)
+        msg = connection.recv_match(
+            type=['GLOBAL_POSITION_INT', 'ATTITUDE', 'SYS_STATUS', 'RC_CHANNELS'],
+            blocking=False
+        )
+
 
         if msg is None:
             continue
 
         timestamp = datetime.now().isoformat()
-
+        
+        #GPS Position
         if msg.get_type() == 'GLOBAL_POSITION_INT':
             lat = msg.lat / 1e7
             lon = msg.lon / 1e7
@@ -44,7 +57,8 @@ with open(log_filename, mode='w', newline='') as logfile:
 
             writer.writerow([timestamp, "GLOBAL_POSITION_INT", lat, lon, alt, "", "", ""])
             logfile.flush()
-
+        
+        #Attitdue
         elif msg.get_type() == 'ATTITUDE':
             roll = msg.roll
             pitch = msg.pitch
@@ -53,3 +67,32 @@ with open(log_filename, mode='w', newline='') as logfile:
 
             writer.writerow([timestamp, "ATTITUDE", "", "", "", roll, pitch, yaw])
             logfile.flush()
+            
+        # Battery
+        elif msg.get_type() == 'SYS_STATUS':
+            voltage = msg.voltage_battery / 1000.0
+            current = msg.current_battery / 100.0
+            remaining = msg.battery_remaining
+            print(f"Battery: {voltage:.2f} V, {current:.2f} A, {remaining}%")
+
+            writer.writerow([timestamp, "SYS_STATUS",
+                             "", "", "",
+                             "", "", "",
+                             voltage, current, remaining,
+                             ""])
+            logfile.flush()
+        
+        #RC Channels
+        elif msg.get_type() == 'RC_CHANNELS':
+            throttle = msg.chan3_raw
+            print(f"Throttle: {throttle}")
+
+            writer.writerow([timestamp, "RC_CHANNELS",
+                             "", "", "",
+                             "", "", "",
+                             "", "", "",
+                             throttle])
+            logfile.flush()
+
+            
+        
